37716e2cab7fc116a6ff26379589d0083ea70c8e
PJS <pjs@MacBook-Pro-von-Paul-2.local>
2023-09-27 11:18:35 +0200

    add pipeline

---
+++ .github/workflows/add_style.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if the file exists
+if [ ! -f "../../index.html" ]; then
+  echo "index.html not found. Please make sure the file exists in the current directory."
+  exit 1
+fi
+
+# Temporary file to store updated content
+temp_file=$(mktemp)
+
+# Add the link tag to the head section
+awk '/<\/head>/ {print "  <link rel=\"stylesheet\" href=\"style.css\">"} 1' ../../index.html > "$temp_file"
+mv "$temp_file" ../../index.html
+
+echo "Stylesheet link added to index.html."
